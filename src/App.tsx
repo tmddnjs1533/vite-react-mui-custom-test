99ddd9b0de51459db91e49e401aadbe54a23e495
tmddnjs1533 <tmddnjs1533@gmail.com>
2022-04-30 22:30:54 +0900

    Layout 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Header } from 'components/Header';
 import Login from './pages/Login';
 import { Button } from 'components/Button';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 function App() {
   return (
     <div className="App">
-      <Header title="hola" />
-      <Button onClick={() => alert('hola')}>Heyo</Button>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <>
+                  <Header title="hola" />
+                  <Button onClick={() => alert('hola')}>Heyo</Button>
+                </>
+              }
+            />
+          </Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
